e6dfe1aaeb66a22ef52bcf62eba06ec670e72470
Luis Enrique G Mtz <elektroacustica@gmail.com>
2015-09-29 15:31:55 -0500

    Quitando libreria de PDF

--- config/app.php
+++ config/app.php
@@ -199,12 +199,10 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
-        'Twig' => TwigBridge\Facade\Twig::class,
-
-
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Debugbar'  => Barryvdh\Debugbar\Facade::class,
+        'Twig'      => TwigBridge\Facade\Twig::class,
     ],
 
 ];
